a37a985f6f5f95762ab08b609af29aa46005658e
Ad-casa-centrale <138740200+Ad-casa-centrale@users.noreply.github.com>
2023-11-24 17:35:35 +0100

    Add files via upload

---
+++ lance_missiles_anti_air.py
@@ -0,0 +1,6 @@
+class AntiairMissileLauncher(Weapon):
+    def_init_(self,ammunitions:int):
+        super()._init_(ammunitions,20)
+    def is_valid_target(self,x:int,y:int):
+        return z>0
+    
